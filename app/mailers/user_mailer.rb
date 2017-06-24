9fff4cd77447fe2dde0aabb53acdb60d97a6c320
Turupawn <ahmed.hn.43@gmail.com>
2017-06-23 19:27:51 -0600

    redo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = "http://3deasy.co/users/sign_in"
-    mail(:to => user.email, :subject => "Welcome to 3dEasy.co")
+    mail(:from => "3DEasy.co <automated@3deasy.co>", :to => user.email, :subject => "Welcome to 3dEasy.co")
   end
 end
