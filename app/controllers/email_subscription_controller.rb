76f3a6a8b959f62c476b8833353ae9e2484607c7
Turupawn <ahmed.hn.43@gmail.com>
2017-07-13 08:57:18 -0600

    unsubscribe

--- app/controllers/email_subscription_controller.rb
+++ app/controllers/email_subscription_controller.rb
@@ -7,11 +7,21 @@ class EmailSubscriptionController < ApplicationController
       return
     end
 
+    @subscription_state = ""
+
     email_subscription = EmailSubscription.find_by(email: email)
 
-    if email_subscription && email_subscription.confirmed
-      redirect_to root_url
-      return
+    if email_subscription
+      if email_subscription.unsubscribed
+        @subscription_state = "unsubscribed"
+        return
+      elsif email_subscription.confirmed
+        @subscription_state = "already confirmed"
+        return
+      else
+        @subscription_state = "error"
+        return
+      end
     end
 
     if !email_subscription
@@ -21,37 +31,51 @@ class EmailSubscriptionController < ApplicationController
     email_subscription.email = email
     email_subscription.key = SecureRandom.hex(25)
     email_subscription.confirmed = false
+    email_subscription.unsubscribed = false
     email_subscription.save
 
     UserMailer.confirm_subscription_email(email_subscription).deliver_later
+
+    @subscription_state = "just subscribed"
   end
 
   def confirm_subscription
     email = params[:email]
     key = params[:key]
 
+    @confirm_state = ""
+
     email_subscription = EmailSubscription.find_by(email: email, key: key)
 
     if !email_subscription
+      @confirm_state = "error"
       return
-      redirect_to root_url
     end
 
     email_subscription.confirmed = true
+    email_subscription.unsubscribed = false
     email_subscription.save
+
+    @confirm_state = "just confirmed"
   end
 
   def unsubscribe
     email = params[:email]
     key = params[:key]
 
+    @unsubscribe_state = ""
+
     email_subscription = EmailSubscription.find_by(email: email, key: key)
 
     if !email_subscription
+      @unsubscribe_state = "error"
       return
-      redirect_to root_url
     end
 
-    email_subscription.destroy
+    email_subscription.confirmed = true
+    email_subscription.unsubscribed = true
+    email_subscription.save
+
+    @unsubscribe_state = "just unsubscribed"
   end
 end
